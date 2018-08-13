ff70b144ae49710bb4d612c2c2b99be28b29ac07
Sascha Dobschal <s.dobschal@tdsoftware.de>
2018-08-13 18:47:34 +0200

    sidebar design

--- client/src/components/partials/Sidebar.vue
+++ client/src/components/partials/Sidebar.vue
@@ -4,6 +4,13 @@
         <nav>
             <ul v-if="!isAuthenticated">
                 <li>
+                    <router-link to="/">{{ $t("navigation.shop") }}</router-link>
+                </li>
+                <li>
+                    <router-link to="/news">{{ $t("navigation.news") }}</router-link>
+                </li>
+                <li></li>
+                <li>
                     <router-link to="/program">{{ $t("navigation.program") }}</router-link>
                 </li>
                 <li>
@@ -21,18 +28,7 @@
                 </li>
                 <li>
                     <router-link to="/images">{{ $t("navigation.images") }}</router-link>
-                </li>
-                <li></li>
-                <li>
-                    <router-link to="/agb">{{ $t("navigation.agb") }}</router-link>
-                </li>
-                <li>
-                    <router-link to="/data">{{ $t("navigation.data") }}</router-link>
-                </li>
-                <li>
-                    <router-link to="/imprint">{{ $t("navigation.imprint") }}</router-link>
-                </li>
-                
+                </li>                
             </ul>
             <ul v-if="isAuthenticated">
                 <li>
@@ -50,6 +46,20 @@
             </ul>
         </nav>
         <div class="wappen"></div>
+        <nav>
+            <ul>
+                <li>
+                    <router-link to="/agb">{{ $t("navigation.agb") }}</router-link>
+                </li>
+                <li>
+                    <router-link to="/data">{{ $t("navigation.data") }}</router-link>
+                </li>
+                <li>
+                    <router-link to="/imprint">{{ $t("navigation.imprint") }}</router-link>
+                </li>
+            </ul>
+        </nav>
+        <div class="banner"></div>
         <div class="footer">
             <router-link to="/login" class="cog-wheel"></router-link>
             <div class="version-number">{{ version }}</div>
@@ -98,25 +108,41 @@ export default {
 .brand
 {
     background-image: url("../../assets/logo.jpg");
-    width: 170px;
-    height: 190px;
-    background-size: 150px auto;
+    width: 150px;
+    height: 166px;
+    background-size: 130px auto;
     background-repeat: no-repeat;
     background-position: 50% 50%;
-    margin: 40px 40px;
+    margin: 32px 50px 32px 50px;
     border: solid 10px white;
     display: block;
+    box-shadow: 0px 10px 15px -10px rgba(0, 0, 38, 0.15);
 }
 
 .wappen
 {
     background-image: url("../../assets/wappen.png");
-    width: 170px;
-    height: 190px;
-    background-size: 170px auto;
+    width: 150px;
+    height: 180px;
+    background-size: 130px auto;
     background-repeat: no-repeat;
     background-position: 50% 50%;
-    margin: 40px 40px;
+    margin: 32px 50px;
+    display: block;
+    box-shadow: 0px 10px 15px -10px rgba(0, 0, 38, 0.15);
+    background-color: white;
+    border: solid 10px white;
+}
+
+.banner
+{
+    background-image: url("../../assets/banner.png");
+    width: 250px;
+    height: 28.12px;
+    background-size: 250px auto;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    margin: 32px 0 0 0;
     display: block;
 }
 
@@ -130,15 +156,15 @@ ul
         min-height: 16px;
         a:link, a:visited
         {
-            @include font2();
+            @include font2();            
             text-decoration: none;
-            text-decoration: none;           
             display: block;
             width: 150px;
             padding: 0px;
-            margin: 0 auto;
-            transition: padding 0.5s $transitionEasing;
+            transition: padding 0.5s cubic-bezier(0.1, 0.5, 0.2, 1);
             position: relative;
+            margin: 0 auto 0 60px;
+            white-space: nowrap;                
             &.router-link-exact-active
             {
                 font-weight: bold;
@@ -189,27 +215,31 @@ ul
 
 .footer
 {
+    background: #181823;
     .cog-wheel
     {
         background: url("../../assets/icon-wheel.svg");
         background-position: 50% 50%;
         background-repeat: no-repeat;
-        margin: 16px;
+        margin: 16px 0px 16px 16px;
         height: 24px;
         width: 24px;
         display: inline-block;
-            vertical-align: bottom;
+        vertical-align: bottom;
+        opacity: 0.5;
         &:hover
         {
+            opacity: 1.0;
             cursor: pointer;
         }
     }
 
     .version-number
     {
-        @include font5( $color: rgba($darkBlue, 0.5) );
+        @include font5( $color: rgba(white, 0.5), $fontWeight: 400, $fontSize: 10px );
         display: inline-block;
-        margin: 16px;
+        margin-top: 17px;
+        vertical-align: top;
     }
 }
 
