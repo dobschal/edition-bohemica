cd439d027618e8dc98dafa464a034ebc7e141ecd
Sascha Dobschal <s.dobschal@tdsoftware.de>
2022-09-19 19:12:00 +0200

    remove captcha from frontend

--- client/src/components/pages/Conact.vue
+++ client/src/components/pages/Conact.vue
@@ -2,8 +2,8 @@
     <div class="page-wrapper">
         <div class="row">
             <div class="col-2"></div>
-            <div class="col-8">                
-                <form @submit.prevent="send" if="isNotSent">                    
+            <div class="col-8">
+                <form @submit.prevent="send" if="isNotSent">
                     <div class="form-group">
                         <label>{{ $t("general.sender") }}</label>
                         <input type="email" placeholder="ihre@email.de" class="form-control" v-model="email">
@@ -16,13 +16,13 @@
                         <label>{{ $t("general.yourMessage") }}</label>
                         <textarea class="form-control" rows="7" v-model="message"></textarea>
                     </div>
-                    <div class="form-group mt-4">
+                    <!-- <div class="form-group mt-4">
                         <vue-recaptcha sitekey="6LfMimwUAAAAAFHmKMcJ3ezGvMaG9uq6kLliVhaX">                            
                         </vue-recaptcha>                        
                         <button type="submit" class="btn btn-primary mt-4">
                             {{ $t("general.send") }}
                         </button>
-                    </div>
+                    </div> -->
                 </form>
             </div>
             <div class="col-2"></div>
@@ -38,8 +38,7 @@ import VueRecaptcha from 'vue-recaptcha';
 
 export default {
     components: { VueRecaptcha },
-    data()
-    {
+    data() {
         return {
             title: "",
             message: "",
@@ -49,31 +48,26 @@ export default {
     },
     methods:
     {
-        async send()
-        {
-            try
-            {
-                if( !this.title || !this.email )
-                {
-                    return toastr.error( this.$t("general.error.missingInput") );
+        async send() {
+            try {
+                if (!this.title || !this.email) {
+                    return toastr.error(this.$t("general.error.missingInput"));
                 }
-                if( !grecaptcha.getResponse() )
-                {
-                    return toastr.error( this.$t("general.captchaMessage") );
-                }
+                // if (!grecaptcha.getResponse()) {
+                //     return toastr.error(this.$t("general.captchaMessage"));
+                // }
                 const response = await HTTP().post("/contact", {
                     title: this.title,
                     message: this.message,
-                    email: this.email,
-                    captcha: grecaptcha.getResponse()
+                    email: this.email
+                    // captcha: grecaptcha.getResponse()
                 });
-                toastr.success( this.$t("contact.sent") );
+                toastr.success(this.$t("contact.sent"));
                 this.$router.push("/");
             }
-            catch(e)
-            {
+            catch (e) {
                 console.error("[Contact] Error on sending contact email: ", e);
-                toastr.error( this.$t("contact.error.send") );
+                toastr.error(this.$t("contact.error.send"));
             }
 
         }
