9e873cc1d6a9d2ba5680fc811bb13e74743ae35b
Sascha Dobschal <saschadobschal@macbookright--sascha.fritz.box>
2024-10-14 15:43:36 +0200

    add uploads folder to backups

--- backup.sh
+++ backup.sh
@@ -6,3 +6,6 @@ docker exec -i mongodb mongodump
 
 # Copy all files out of the container into a backup folder
 docker cp mongodb:/dump /root/backups/mongodb
+
+# Copy all uploaded files
+docker cp edition-bohemica-app-1:/usr/src/app/server/uploads /root/backups/uploads-edition-bohemica
